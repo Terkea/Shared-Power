04cc6600746920b1f66a861c0683e660ec1d693f
Terkea <terkeabt@gmail.com>
2019-11-26 17:36:30 +0000

    main frame + swap frame functionality

--- app/view/app.py
+++ app/view/app.py
@@ -5,8 +5,73 @@ from tkinter import Frame, Label, Entry, Button
 class App(tk.Frame):
 
     def __init__(self, root):
+        # create a new frame
         tk.Frame.__init__(self, root)
-        # window size
-        root.geometry('600x400')
+        # frame size
+        root.geometry('1000x500')
 
-        tk.Label(self, text="app page").pack(side="top", fill="x", pady=5)
+        # both width and height are not resizable
+        root.resizable(False, False)
+
+        # column, row sizes
+        root.grid_columnconfigure(1, weight=1)
+        root.grid_rowconfigure(0, weight=1)
+
+        # set the content frame size to 800
+        self.grid_columnconfigure(1, minsize=800)
+
+        # create the frames
+        self.users_menu_frame = Frame(self, background='red')
+        self.welcome_frame = Frame(self, background='blue')
+        self.view_profile = Frame(self, background='green')
+
+        # this is the frame that the user will see when they log in
+        self.content_frame = self.welcome_frame
+
+
+        # column/row configurations for menu frame
+        self.users_menu_frame.grid_columnconfigure(0, minsize=150)
+        self.users_menu_frame.grid_rowconfigure(0, minsize=50)
+        self.users_menu_frame.grid_rowconfigure(1, minsize=40)
+        self.users_menu_frame.grid_rowconfigure(2, minsize=40)
+        self.users_menu_frame.grid_rowconfigure(3, minsize=40)
+        self.users_menu_frame.grid_rowconfigure(4, minsize=40)
+        self.users_menu_frame.grid_rowconfigure(5, minsize=40)
+
+        # items for menu frame
+        hello_label = tk.Label(self.users_menu_frame, text='Hello, User').grid(column=0, row=0, sticky='nswe')
+
+        view_profile_button = tk.Button(self.users_menu_frame,
+                                        text="View Profile", command=lambda: self.switch_frame(self.view_profile))\
+                                        .grid(column=0, row=1, sticky='nswe')
+
+        invoices_button = tk.Button(self.users_menu_frame, command=lambda: self.switch_frame(self.welcome_frame),
+                                    text="Invoices").grid(column=0, row=2, sticky='nswe')
+        bookings_button = tk.Button(self.users_menu_frame, text="View bookings").grid(column=0, row=3, sticky='nswe')
+        tools_button = tk.Button(self.users_menu_frame, text="Search tools").grid(column=0, row=4, sticky='nswe')
+        basket_button = tk.Button(self.users_menu_frame, text="Basket").grid(column=0, row=5, sticky='nswe')
+
+
+
+        welcome_label = Label(self.welcome_frame, text='Hello, madafaka')
+        welcome_label.grid(column=0, row=0)
+
+        profile_label = Label(self.view_profile, text='this is the profile of a fucker')
+        profile_label.grid(column=0, row=0)
+
+
+
+        # the position of the frames in the main one
+        self.users_menu_frame.grid(column=0, row=0, sticky='nswe')
+        self.content_frame.grid(column=1, row=0, sticky='nswe')
+
+
+    def switch_frame(self, frame):
+        """
+        swaps the content frame in view with the one passed as param
+        :param frame: Frame
+        """
+        if self.content_frame is not frame:
+            self.content_frame.grid_forget()
+            self.content_frame = frame
+            self.content_frame.grid(column=1, row=0, sticky='nswe')
